8f0acaaf2dc1093753d46416636f727aee7f7d7d
pluka073 <71723381+pluka073@users.noreply.github.com>
2022-10-18 14:21:48 -0600

    Try #1 on A1

--- SearchPage.php
+++ SearchPage.php
@@ -5,40 +5,56 @@
    
 <head>
     <meta charset="utf-8"/>  
-    <title>Single Song</title>   
+    <title>Search Page</title>   
     
 </head>
 <body>
         <header>Header</header>
     <input type="radio" id="title" name="Title">
     <label for="title">Title</label>
-    <select>
-<?php 
- 
-$connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME); 
-if ( mysqli_connect_errno() ) { 
- die( mysqli_connect_error() ); 
-} 
- 
-$sql = "select * from songs order by artist_id"; 
- 
-if ($result = mysqli_query($connection, $sql)) { 
- while($row = mysqli_fetch_assoc($result)) 
- { 
- echo '<option value="' . $row['title'] . '">'; 
- echo $row['title']; 
- echo "</option>"; 
- } 
-    
- // release the memory used by the result set
- mysqli_free_result($result); 
-}
- 
-// close the database connection
-mysqli_close($connection); 
 
-?> 
-</select>
+<?php 
+    if ( isset($_POST['search']) ) { 
+ $search = findTitle($_POST['search']); 
+} 
+ function findTitle($search) { 
+ try { 
+ $pdo = new PDO(DBCONNSTRING,DBUSER,DBPASS); 
+ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+ $sql = "SELECT title FROM songs"; 
+ $sql .= " WHERE Title LIKE '%$title%'";  
+ 
+ $result = $pdo->query($sql); 
+ $search = $result->fetchAll(PDO::FETCH_ASSOC); 
+ $pdo = null; 
+ return $title; 
+ } 
+     catch (PDOException $e){
+         die( $e->getMessage());
+     }
+ } 
+ ?>
+    <section class="twelve wide column">
+<?php
+     function outputTitle($title) { 
+ foreach ($tit as $row) { 
+ echo $row['Title'] . " (" . $row['YearOfWork'] . ")<br/>"; 
+ } 
+} 
+ /* add your PHP code here */
+ if ( isset($_POST['search']) ) { 
+ if ( count($title) > 0 ) { 
+ outputTitle($title); 
+ } else { 
+ echo "no titles found with search term = " . 
+ $_POST['search']; 
+ } 
+ }
+?>
+<div><input type="text" placeholder="enter search title" name="search" />
+</div>   
+</section>
+    
     <br>
     <input type="radio" id="artist" name="Artist">
     <label for="artist">Artist</label>
@@ -105,8 +121,9 @@ mysqli_close($connection);
     <input type="radio" id="pop" name="popularity">
     <label for="pop">Popularity</label>
     <br>
-    <button>Search
-    </button>
+     <button type="submit">
+            Filter 
+          </button>
        
     </body>
 </html>
--- SingleSong.php
+++ SingleSong.php
@@ -1,26 +1,26 @@
 <?php require_once('config.inc.php'); ?>
+
 <!DOCTYPE html>
 <html>
 <body>
 <h1>Database Tester (mysqli)</h1>
 Genre: 
-<select>
 <?php 
  
 $connection = mysqli_connect(DBHOST, DBUSER, DBPASS, DBNAME); 
 if ( mysqli_connect_errno() ) { 
  die( mysqli_connect_error() ); 
 } 
- 
-$sql = "select * from genres"; 
- 
+$sql = "select Title, artists.artist_name, types.type_name, genres.genres_name, year, duration 
+        from songs INNER JOIN songs ON genres = songs.genre_id 
+                    INNER JOIN songs ON artists.artist_id = songs.artist_id 
+                    INNER JOIN artists ON types.type_id = artists.artist_type_id
+        where song_id = $song_id";
+    
 if ($result = mysqli_query($connection, $sql)) { 
- while($row = mysqli_fetch_assoc($result)) 
- { 
- echo '<option value="' . $row['GenreID'] . '">'; 
- echo $row['GenreName']; 
- echo "</option>"; 
- } 
+ foreach ($result as $row) { 
+ echo $row[0] . " - " . $row['LastName'] . "<br/>"; 
+}
  // release the memory used by the result set
  mysqli_free_result($result); 
 } 
@@ -28,19 +28,7 @@ if ($result = mysqli_query($connection, $sql)) {
 // close the database connection
 mysqli_close($connection); 
 ?> 
-</select>
-</body>
-</html>
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="utf-8"/>  
-    <title>Single Song</title>   
     
-</head>
-<body>
-    <header>Header</header>
-    <div>Song Information</div>
     <p>title, name, artist, genre, year, duration</p>
     <br>
     <h2>Analysis Data:</h2>
